3033522bbef6f1d2622cc694f7aa51ae5b626e90
siddh-m <henry-sm>
2024-05-20 19:47:06 +0530

    did something reading the docs

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Randomizes the search engine used when a query is inserted into address bar of a browser window.
 
-~~also an excuse to learn and do something in js~~
+~~also this is my first time making something proper so most of it is blindly following docs and googling instead og doind something of my own~~
--- action/background.js
+++ action/background.js
@@ -1,8 +1,19 @@
-chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-  if (text.startsWith('${chrome.omnibox.keyword} ')){
-    suggest([{
-        content: `${chrome.omnibox.keyword} ${userQuery}`,
-        description: `So you want to search for "${userQuery}"`
-    }])
-  }  
+chrome.omnibox.onInputStarted.addListener(function(){
+  console.log('event started...'); //idk what this does but docs said so
 });
+
+chrome.omnibox.onInputEntered.addListener(function(text){
+    console.log(text);
+  chrome.tabs.create({url: 'https://www.reddit.com/search/?q='+text})
+});
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest){
+
+
+  var suggestions = [];
+  suggestions.push({ deletable: true, content: "https://www.google.com/search/?q=" + text, description: '(10^100) '+text });
+  suggestions.push({ deletable: true, content: "https://bing.com/search?q=" + text, description: '(bing Chilling) '+text });
+
+  suggest(suggestions);
+});
+
