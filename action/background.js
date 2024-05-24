adc5e918e45638ac3fd982b187c878e7abb34ff0
siddh-m <henry-sm>
2024-05-24 14:38:27 +0530

    working prototype of sorts

--- action/background.js
+++ action/background.js
@@ -1,19 +1,30 @@
 chrome.omnibox.onInputStarted.addListener(function(){
-  console.log('event started...'); //idk what this does but docs said so
+  console.log("we're going to search"); //idk what this does but docs sai
 });
 
 chrome.omnibox.onInputEntered.addListener(function(text){
-    console.log(text);
-  chrome.tabs.create({url: 'https://www.reddit.com/search/?q='+text})
+    fetch('./engines.json')
+        .then(response => response.json())
+        .then(data => {
+        const arrayLength = data.search_engines.length;
+        console.log(arrayLength);
+        var nth = new Date().getTime()
+        nth = nth% arrayLength
+        chrome.tabs.update({url: 'https://www.'+data.search_engines[nth]+'?q='+text})
+        })
+        .catch(error => {
+        console.error("Error fetching data:", error);
+        });
+
 });
 
 chrome.omnibox.onInputChanged.addListener(function(text, suggest){
 
-
+/*
   var suggestions = [];
   suggestions.push({ deletable: true, content: "https://www.google.com/search/?q=" + text, description: '(10^100) '+text });
-  suggestions.push({ deletable: true, content: "https://bing.com/search?q=" + text, description: '(bing Chilling) '+text });
 
   suggest(suggestions);
+*/
 });
 
